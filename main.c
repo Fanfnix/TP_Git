a82487e99a6a9e1d162e488968f5e33f9ca5afed
Fanfnix <valentin@familledenis.net>
2024-10-21 10:22:48 +0200

    commit Ex1, 2, 3

--- main.c
+++ main.c
@@ -16,28 +16,75 @@ void initGrille(int grille[9][9], int n)
 }
 // Fin EX1 //
 
+// EX2 //
+
+void remplirGrille(int grille[9][9], int n)
+{
+    int run = 1;
+    int l, c, valeur;
+    while (run)
+    {
+        afficherGrille(grille, n);
+        do 
+        {
+            printf("Ligne : ");
+            scanf(" %d", &l);
+            printf("Colonne : ");
+            scanf(" %d", &c);
+        } while (c > 8 || c < 0 || l > 8 || l < 0);
+        printf("Valeur : ");
+        scanf(" %d", &valeur);
+
+        grille[l][c] = valeur;
+
+        printf("Entrez une autre valeur ? (1/0) ");
+        scanf(" %d", &run);
+    }
+}
+// Fin EX2 //
+
 // EX3 //
+
 void afficherGrille(int grille[9][9], int n)
 {
+    for(int i = 0; i < n; i++)
+    {
+        printf("---");
+    }
+    printf("\n");
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
         {
-            printf(" %d ", grille[i][j]);
+            if (grille[i][j] != 0)
+            {
+                printf(" %d ", grille[i][j]);
+            }
+            else
+            {
+                printf("   ");
+            }
         }
         printf("\n");
     }
+    for(int i = 0; i < n; i++)
+    {
+        printf("---");
+    }
+    printf("\n");
 }
 // Fin EX3 //
 
 int main()
 {
-	int n = 5;
+	int n = 9;
     int grille[9][9];
 
     initGrille(grille, n);
-    
+
     afficherGrille(grille, n);
+
+    remplirGrille(grille, n);
     
     int grille_facile[9][9] = {
 		{6, 0, 4, 9, 0, 0, 3, 0, 8},
