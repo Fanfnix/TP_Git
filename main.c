fbad8e2b4aa0afdcda9a9fa25809d613ccc18962
Fanfnix <valentin@familledenis.net>
2024-10-21 09:53:28 +0200

    Commit EX1

---
+++ main.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <string.h>
+
+
+// EX1 //
+
+void initGrille(int grille[9][9], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            grille[i][j] = 0;
+        }
+    }
+}
+// Fin EX1 //
+
+// EX3 //
+void afficherGrille(int grille[9][9], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            printf(" %d ", grille[i][j]);
+        }
+        printf("\n");
+    }
+}
+// Fin EX3 //
+
+int main()
+{
+	int n = 5;
+    int grille[9][9];
+
+    initGrille(grille, n);
+    
+    afficherGrille(grille, n);
+    
+    int grille_facile[9][9] = {
+		{6, 0, 4, 9, 0, 0, 3, 0, 8},
+		{3, 2, 0, 8, 0, 0, 4, 0, 0},
+		{0, 0, 0, 6, 0, 4, 5, 2, 1},
+		{1, 0, 2, 7, 4, 3, 8, 9, 6},
+		{8, 0, 0, 0, 0, 0, 0, 0, 0},
+		{9, 4, 7, 5, 8, 0, 0, 0, 0},
+		{4, 9, 0, 2, 0, 8, 6, 0, 3},
+		{0, 3, 6, 4, 0, 0, 0, 0, 0},
+		{0, 7, 0, 0, 6, 9, 0, 0, 4}
+	};
+
+	int grille_moyenne[9][9] = {
+		{1, 0, 0, 7, 0, 6, 0, 0, 0},
+		{0, 0, 0, 0, 0, 8, 5, 6, 0},
+		{6, 0, 8, 0, 5, 3, 4, 0, 1},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 6},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 3, 0, 0, 0, 0, 0, 0, 0},
+		{8, 4, 0, 3, 0, 1, 0, 5, 9}
+	};
+
+	return 0;
+}
